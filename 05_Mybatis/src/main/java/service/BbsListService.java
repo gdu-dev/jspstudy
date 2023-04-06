80d3bf91b326464cf23aaef968efe2f3065bf83d
gdu-dev <minkt78@naver.com>
2023-04-06 09:48:45 +0900

    2023-04-06

--- 05_Mybatis/src/main/java/service/BbsListService.java
+++ 05_Mybatis/src/main/java/service/BbsListService.java
@@ -4,13 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.ActionForward;
+import repository.BbsDAO;
 
 public class BbsListService implements IBbsService {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		request.setAttribute("bbsList", BbsDAO.getInstance().selectAllBbsList());
+		return new ActionForward("bbs/list.jsp", false);
 	}
 
 }
