fee2c689256904dd4c6ac9ff32f79a4788a7d719
gdu-dev <minkt78@naver.com>
2024-02-28 10:07:10 +0900

    20240228

--- 01_servlet/src/main/java/pkg02_request/MyRequest.java
+++ 01_servlet/src/main/java/pkg02_request/MyRequest.java
@@ -1,6 +1,7 @@
 package pkg02_request;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -17,12 +18,21 @@ public class MyRequest extends HttpServlet {
     request.setCharacterEncoding("UTF-8");
     
     // 2. 요청 파라미터
+    //  1) 모든 파라미터는 String 타입이다.
+    //  2) 파라미터가 없으면 null 이다.
+    //  3) 파라미터의 값이 없으면 ""(빈 문자열) 이다.
+    
+    /* if 문을 이용한 [null & 빈 문자열] 처리 */
     String strNumber = request.getParameter("number");
-    int number = Integer.parseInt(strNumber);
+    int number = 0;
+    if(strNumber != null && !strNumber.isEmpty())
+      number = Integer.parseInt(strNumber);
     System.out.println(number);
     
+    /* Optional<T> 클래스를 이용한 [null & 빈 문자열] 처리 */
     String strNumber2 = request.getParameter("number2");
-    double number2 = Double.parseDouble(strNumber2);
+    Optional<String> opt = Optional.ofNullable(strNumber2);
+    double number2 = Double.parseDouble(opt.orElse("0").isEmpty() ? "0" : strNumber2);
     System.out.println(number2);
     
   }
