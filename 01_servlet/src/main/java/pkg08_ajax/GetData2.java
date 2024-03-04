2e41fcf2fcf3816aed6b5e39e0274af15f54f131
gdu-dev <minkt78@naver.com>
2024-03-04 12:41:01 +0900

    20240304

--- 01_servlet/src/main/java/pkg08_ajax/GetData2.java
+++ 01_servlet/src/main/java/pkg08_ajax/GetData2.java
@@ -1,6 +1,8 @@
 package pkg08_ajax;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -20,7 +22,7 @@ public class GetData2 extends HttpServlet {
     // 도로교통공단_사망교통사고정보서비스 : 사망교통사고정보 Rest 조회
     StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/B552061/AccidentDeath/getRestTrafficAccidentDeath");
     urlBuilder.append("?serviceKey=").append(URLEncoder.encode("bEQBRPHjt0tZrc7EsL0T8usfsZ1+wT+5jqamBef/ErC/5ZO6N7nYdRmrwR91bh5d3I1AQeY5qdbJOF6Kv0U1CQ==", "UTF-8"));
-    urlBuilder.append("&searchYear=").append(URLEncoder.encode("2023", "UTF-8"));
+    urlBuilder.append("&searchYear=").append(URLEncoder.encode("2022", "UTF-8"));
     urlBuilder.append("&siDo=").append(URLEncoder.encode("1100", "UTF-8"));
     urlBuilder.append("&guGun=").append(URLEncoder.encode("1116", "UTF-8"));
     urlBuilder.append("&type=").append(URLEncoder.encode("json", "UTF-8"));
@@ -46,6 +48,29 @@ public class GetData2 extends HttpServlet {
       if(responseCode != HttpURLConnection.HTTP_OK) {
         throw new RuntimeException("API 응답 실패");
       }
+      
+      // 문자 입력 스트림 (사망 사고 정보 읽기)
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      
+      // StringBuilder 객체 (전체 JSON)
+      StringBuilder builder = new StringBuilder();
+      
+      // String 객체 (한 줄)
+      String line = null;
+      
+      // 문자 입력 스트림을 통해 사망 사고 정보 읽어 오기
+      while((line = in.readLine()) != null) {
+        builder.append(line);
+      }
+      
+      // 응답 데이터 타입 & 인코딩
+      response.setContentType("application/json; charset=UTF-8");
+      
+      // 응답
+      PrintWriter out = response.getWriter();
+      out.print(builder.toString());
+      out.flush();
+      out.close();
       
     } catch (Exception e) {
       // $.ajax().fail() 메소드로 전달되는 응답 만들기
