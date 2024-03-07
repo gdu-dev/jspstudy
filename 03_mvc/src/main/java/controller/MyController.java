ec8e5418b51420dae8d68e9442574315c86d1ed5
gdu-dev <minkt78@naver.com>
2024-03-07 10:52:15 +0900

    20240307

--- 03_mvc/src/main/java/controller/MyController.java
+++ 03_mvc/src/main/java/controller/MyController.java
@@ -13,7 +13,15 @@ public class MyController extends HttpServlet {
   
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
   
-    System.out.println("hello");
+    /* 요청 UTF-8 인코딩 */
+    request.setCharacterEncoding("UTF-8");
+    
+    /* 어떤 요청인지 확인 (URLMapping 확인) */
+    String requestURI = request.getRequestURI();    /* http://localhost:8080/mvc/getDate.do */
+    String contextPath = request.getContextPath();  /* /mvc */
+    String urlMapping = requestURI.substring(requestURI.indexOf(contextPath) + contextPath.length());
+    
+    System.out.println(urlMapping);
   
   }
   
